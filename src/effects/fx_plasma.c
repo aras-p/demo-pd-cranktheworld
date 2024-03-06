34d26711c63c57867c7714fd3d2c42f145e8547e
Aras Pranckevicius <aras@nesnausk.org>
2024-03-06 13:25:03 +0200

    Plasma effect

---
+++ src/effects/fx_plasma.c
@@ -0,0 +1,82 @@
+#include "fx.h"
+
+#include "pd_api.h"
+#include "../mathlib.h"
+
+#define TRIG_TABLE_SIZE 512
+#define TRIG_TABLE_MASK (TRIG_TABLE_SIZE-1)
+#define TRIG_TABLE_SCALE 1024
+static int s_sin_table[TRIG_TABLE_SIZE];
+
+static void init_sin_table()
+{
+	const float idx_to_rad = 2.0f * M_PIf / TRIG_TABLE_SIZE;
+	for (int i = 0; i < TRIG_TABLE_SIZE; ++i)
+	{
+		float rad = i * idx_to_rad;
+		s_sin_table[i] = (int)(sinf(rad) * TRIG_TABLE_SCALE);
+	}
+}
+
+static uint32_t s_rng = 1;
+
+static uint16_t s_plasma_pos1;
+static uint16_t s_plasma_pos2;
+static uint16_t s_plasma_pos3;
+static uint16_t s_plasma_pos4;
+
+static int fx_plasma_update(uint32_t buttons_cur, float crank_angle, float time, uint8_t* framebuffer, int framebuffer_stride)
+{
+	s_rng = 1;
+	int tpos4 = s_plasma_pos4;
+	int tpos3 = s_plasma_pos3;
+
+	for (int py = 0; py < LCD_ROWS; ++py)
+	{
+		int tpos1 = s_plasma_pos1 + 5;
+		int tpos2 = s_plasma_pos2 + 3;
+
+		tpos3 &= TRIG_TABLE_MASK;
+		tpos4 &= TRIG_TABLE_MASK;
+
+		uint8_t* row = framebuffer + py * framebuffer_stride;
+
+		for (int px = 0; px < LCD_COLUMNS; ++px)
+		{
+			tpos1 &= TRIG_TABLE_MASK;
+			tpos2 &= TRIG_TABLE_MASK;
+
+			int plasma = s_sin_table[tpos1] + s_sin_table[tpos2] + s_sin_table[tpos3] + s_sin_table[tpos4];
+
+			int index = plasma >> 3;
+			index &= 0xFF;
+			//if (index < 0) index = 0;
+			//if (index > 255) index = 255;
+			int test = XorShift32(&s_rng) & 0xFF;
+			if (index < test) {
+				uint8_t mask = ~(1 << (7 - (px & 7)));
+				row[px >> 3] &= mask;
+			}
+
+			tpos1 += 5;
+			tpos2 += 3;
+		}
+
+		tpos4 += 3;
+		tpos3 += 1;
+	}
+
+	s_plasma_pos1 += 7;
+	s_plasma_pos3 += 3;
+
+	//s_plasma_pos2 += 17;
+	//s_plasma_pos4 += 17;
+
+	return 0;
+}
+
+Effect fx_plasma_init()
+{
+	init_sin_table();
+	return (Effect) {fx_plasma_update};
+}
--- src/main.c
+++ src/main.c
@@ -8,15 +8,17 @@ typedef enum
 {
 	kFxPlanes = 0,
 	kFxStarfield,
+	kFxPlasma,
 	kFxCount,
 } EffectType;
 
 static Effect s_effects[kFxCount];
 
-static EffectType s_cur_effect = 0;
+static EffectType s_cur_effect = kFxPlasma;
 
 Effect fx_planes_init();
 Effect fx_starfield_init();
+Effect fx_plasma_init();
 
 
 static int update(void* userdata);
@@ -40,6 +42,7 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 
 		s_effects[kFxPlanes] = fx_planes_init();
 		s_effects[kFxStarfield] = fx_starfield_init();
+		s_effects[kFxPlasma] = fx_plasma_init();
 
 		pd->system->resetElapsedTime();
 		pd->system->setUpdateCallback(update, pd);
@@ -70,7 +73,7 @@ static int update(void* userdata)
 	int dbg_value = 0;
 	dbg_value = s_effects[s_cur_effect].update(btCur, pd->system->getCrankAngle(), pd->system->getElapsedTime(), pd->graphics->getFrame(), LCD_ROWSIZE);
 
-	pd->graphics->fillRect(0, 0, 30, 32, kColorWhite);
+	pd->graphics->fillRect(0, 0, 40, 32, kColorWhite);
 
 	char buf[100];
 	buf[0] = dbg_value / 10000 % 10 + '0';
