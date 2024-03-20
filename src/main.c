756bb0e32f2e87d48e00786058adcc1e7e1a5b61
Aras Pranckevicius <aras@nesnausk.org>
2024-03-20 11:45:12 +0200

    xm music playback

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include "allocator.h"
 #include "util/pixel_ops.h"
 
+//#define PLAY_MUSIC_MP3 1
+#define PLAY_MUSIC_XM 1
+
 typedef enum
 {
 	kFxPlanes = 0,
@@ -36,8 +39,23 @@ static int update(void* userdata);
 const char* fontpath = "/System/Fonts/Roobert-10-Bold.pft";
 LCDFont* font = NULL;
 
-//static const char* kMusicPath = "Music.mp3";
-//static FilePlayer* s_music;
+#if PLAY_MUSIC_MP3
+static const char* kMusicPath = "Music.mp3";
+static FilePlayer* s_music;
+#endif
+#if PLAY_MUSIC_XM
+#include "external/libxm/include/xm.h"
+static const char* kMusicPath = "cybercul.xm";
+static xm_context_t* s_music_player;
+static char* s_music_data;
+static SoundSource* s_music;
+
+static int xm_AudioSourceFn(void* context, int16_t* left, int16_t* right, int len)
+{
+	xm_generate_samples_16_mono(s_music_player, left, len);
+	return 1;
+}
+#endif
 
 #ifdef _WINDLL
 __declspec(dllexport)
@@ -46,19 +64,44 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 {
 	if (event == kEventInit)
 	{
+		pd_realloc = pd->system->realloc;
+
 		const char* err;
 		font = pd->graphics->loadFont(fontpath, &err);	
 		if (font == NULL)
 			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
 
-		//s_music = pd->sound->fileplayer->newPlayer();
-		//int music_ok = pd->sound->fileplayer->loadIntoPlayer(s_music, kMusicPath);
-		//if (!music_ok) {
-		//	pd->system->error("Could not load music file %s", kMusicPath);
-		//}
-		//pd->sound->fileplayer->play(s_music, 0);
+#		if PLAY_MUSIC_MP3
+		s_music = pd->sound->fileplayer->newPlayer();
+		int music_ok = pd->sound->fileplayer->loadIntoPlayer(s_music, kMusicPath);
+		if (!music_ok) {
+			pd->system->error("Could not load music file %s", kMusicPath);
+		}
+		pd->sound->fileplayer->play(s_music, 0);
+#		endif
 
-		pd_realloc = pd->system->realloc;
+#		if PLAY_MUSIC_XM
+		SDFile* xm_file = pd->file->open(kMusicPath, kFileRead);
+		if (!xm_file) {
+			pd->system->error("Could not load music file %s", kMusicPath);
+		}
+		else {
+			FileStat st = {0};
+			pd->file->stat(kMusicPath, &st);
+			s_music_data = pd_malloc(st.size);
+			pd->file->read(xm_file, s_music_data, st.size);
+			pd->file->close(xm_file);
+
+			int music_ok = xm_create_context_safe(&s_music_player, s_music_data, st.size, 44100);
+			if (music_ok != 0) {
+				pd->system->error("Could not play music file %s err %i", kMusicPath, music_ok);
+			}
+			else {
+				s_music = pd->sound->addSource(xm_AudioSourceFn, NULL, 0);
+			}
+		}
+
+#		endif
 
 		init_blue_noise(pd);
 
