531b152446e1f166451351bea25df1cba4a1bb8a
Aras Pranckevicius <aras@nesnausk.org>
2024-04-09 13:39:42 +0300

    Test: Xor Towers at 10fps

--- src/effects/fx_various_test.c
+++ src/effects/fx_various_test.c
@@ -4,16 +4,21 @@
 #include "../mathlib.h"
 #include "../util/pixel_ops.h"
 
-// "Pretty Hip" by Fabrice Neyret https://www.shadertoy.com/view/XsBfRW 24fps at 2x2t
+#define TEST_PRETTY_HIP 0 // "Pretty Hip" by Fabrice Neyret https://www.shadertoy.com/view/XsBfRW - 24fps at 2x2t
+#define TEST_XOR_TOWERS 1 // "XOR Towers" by Greg Rostami https://www.shadertoy.com/view/7lsXR2 simplified - 10fps at 2x2t
+
+#define CURRENT_TEST TEST_XOR_TOWERS
 
 typedef struct TraceState
 {
 	float t;
 	float rotmx, rotmy;
+	float camdist;
 } TraceState;
 
 static int trace_ray(TraceState* st, float x, float y)
 {
+#if CURRENT_TEST == TEST_PRETTY_HIP
 	float ux = st->rotmy * x + st->rotmx * y;
 	float uy = st->rotmx * x - st->rotmy * y;
 	ux = ux * 10.0f + 5.0f;
@@ -31,7 +36,44 @@ static int trace_ray(TraceState* st, float x, float y)
 	float a = smoothstep(-0.05f, 0.0f, b) + s * 0.1f;
 
 	return MIN(255, a * 250.0f);
+#endif // CURRENT_TEST == TEST_PRETTY_HIP
+
+#if CURRENT_TEST == TEST_XOR_TOWERS
+	float ux = st->rotmy * x + st->rotmx * y;
+	float uy = st->rotmx * x - st->rotmy * y;
+	ux *= 1.666f;
+	uy *= 1.666f;
+
+	// camera motion
+	float cx = 7.0f * st->t;
+	float cy = 30.0f * st->t;
+
+	int bx = 0, by = 0, bz = 0;
+
+#define MAXSTEP 35
+	float height = 0.0f;
+	int it = 0;
+	float heightstep = 0.3f;
+	for (; it < MAXSTEP; ++it)
+	{
+		int tst = bx ^ by ^ bz;
+		tst &= 63;
+		if (tst < bz - 8)
+			break;
+		bx = (int)(ux * height + cx);
+		by = (int)(uy * height + cy);
+		bz = (int)height;
+		height += heightstep;
+		heightstep += 0.07f;
+	}
+	return (int)(height * (255.0f / 70.0f));
+
+#endif // CURRENT_TEST == TEST_XOR_TOWERS
+
+	return 128;
 }
+
+
 
 static int s_frame_count = 0;
 
@@ -39,8 +81,17 @@ static int fx_various_test_update(uint32_t buttons_cur, uint32_t buttons_pressed
 {
 	TraceState st;
 	st.t = time;
+#if CURRENT_TEST == TEST_PRETTY_HIP
 	st.rotmx = 0.7f;//sinf(st.t / 4.0f);
 	st.rotmy = 0.7f;//cosf(st.t / 4.0f);
+#endif
+#if CURRENT_TEST == TEST_XOR_TOWERS
+	st.t = time * 0.1f;
+	float r_angle = 0.6f - 0.1f * st.t;
+	st.rotmx = sinf(r_angle);
+	st.rotmy = cosf(r_angle);
+	st.camdist = 2.0f + cosf(st.t * 1.7f);
+#endif
 
 	float xsize = 1.0f;
 	float ysize = 0.6f;
